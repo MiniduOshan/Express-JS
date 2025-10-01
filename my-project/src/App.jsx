ac08dfb58254389adb56130a76a7aca9b81dadfd
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-10-01 22:18:45 +0530

    init

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './components/form'
+import UserTable from './components/userTable'
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <UserTable />
+      
+    </div>
+  )
+}
+
+export default App
