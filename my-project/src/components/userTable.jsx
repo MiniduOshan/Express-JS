fc36176ffe23a56cef74773a518d0ed9276a1412
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-10-02 16:39:30 +0530

    table fix

--- my-project/src/components/userTable.jsx
+++ my-project/src/components/userTable.jsx
@@ -6,16 +6,20 @@ function UserTable() {
     <div className='flex flex-col items-center mt-10 ml-10 mr-10'>
       <h2 className='text-center text-2xl font-bold my-6'>User List</h2>
       <table className='min-w-full border border-gray-300'>
+            <thead>
             <tr className='bg-gray-200'>
               <th className='border border-gray-300 p-2'>First Name</th>
               <th className='border border-gray-300 p-2'>Last Name</th>
               <th className='border border-gray-300 p-2'>Email</th>
             </tr>
+            </thead>
+            <tbody>
             <tr>
               <td>{Form.firstName}</td>
               <td>{Form.lastName}</td>
               <td>{Form.email}</td>
             </tr>
+            </tbody>
        </table>     
     </div>
   )
