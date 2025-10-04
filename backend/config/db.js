c483b2c8b2312c9860b5bf4ea3439f7cc4bf6226
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-10-04 08:36:51 +0530

    schema

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,6 @@ export const connectDB=async()=>{
       await mongoose.connect(process.env.MONGO_URI);
       console.log("Mongo Connected");
 
-      }catch(error){
       console.log("DB connection failed:", error.message);
       
       }
